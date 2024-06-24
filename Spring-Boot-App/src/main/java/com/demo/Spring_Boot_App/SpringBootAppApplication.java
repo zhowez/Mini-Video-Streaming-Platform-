7726c0bf4c8177b9fa258aced73ee609635cdf74
James Howes <zhowes14@icloud.com>
2024-06-24 18:40:21 -0400

    Phase 1: Created 3 docker Containers for the database, backend and Rabbit

---
+++ Spring-Boot-App/src/main/java/com/demo/Spring_Boot_App/SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Spring_Boot_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-App/src/test/java/com/demo/Spring_Boot_App/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Spring_Boot_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
